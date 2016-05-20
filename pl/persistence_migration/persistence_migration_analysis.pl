7223b2373f629174f1138cda02d527ac8003b54a
Alessandro Borges Rodrigues <aborgesrodrigues@gmail.com>
2016-05-20 19:18:23 -0300

    correction of check of subclass call

--- pl/persistence_migration/persistence_migration_analysis.pl
+++ pl/persistence_migration/persistence_migration_analysis.pl
@@ -3,27 +3,15 @@
 %% sysout_call(CallId, CallParent, Enclosing, FieldAccess, StaticTypeRef, Argument)
 persistence_call(CallId, MethodCall, MethodCalled, MethodCalledName, MethodCalledParameters, MethodCalledReturnType, MethodCalledExceptions, CallParameters, DAO, Business, NotBusiness, GenericDAO, DAOClass, BusinessClass, GenericDAOClass) :-
   callT(CallId, _, MethodCall, Receiver, CallParameters, MethodCalled, _, _),
-  methodT(MethodCalled, DAO, MethodCalledName, MethodCalledParameters, MethodCalledReturnType, MethodCalledExceptions, _, _),
+  methodT(MethodCalled, _, MethodCalledName, MethodCalledParameters, MethodCalledReturnType, MethodCalledExceptions, _, _),
   fully_qualified_name(GenericDAO, GenericDAOClass),
+  ((identT(Receiver, CallId, MethodCall, Local), localT(Local, _, _, DAO, _, _)) ; newT(Receiver, _, _, _, _, _, _, DAO1, _)),
+  fully_qualified_name(DAO,DAOClass),
   %subtype(DAO, GenericDAO),
-  ((identT(Receiver, CallId, MethodCall, Local), localT(Local, _, _, DAO1, _, _)) ; newT(Receiver, _, _, _, _, _, _, DAO1, _)),
-  fully_qualified_name(DAO1,DAOClass),
-  %classT(DAO, _, NameClass, _, _),
-  %classT(DAO2, _, NameClass2, _, _),
-  %enclClass(DAO, GenericDAO),
-  %extendsT(_, DAO, GenericDAO),
-  %fully_qualified_name(DAO1,DAOClass),
-  %subtype(DAO1, GenericDAO),
-  %NameClass == NameClass2,
   % limit the scope
   methodT(MethodCall, Business, _, _, _, _, _, _),
   not(fully_qualified_name(Business, BusinessClass)),
   not(subtype(Business, GenericDAO)),
   fully_qualified_name(NotBusiness, BusinessClass).
-%  classT(Business,_,_,_,_),
-%  classT(DAOClasses,_,_,_,_),
-%  subtype(DAOClasses, GenericDAO),
-%  Business \== DAOClasses,
-%  Business \== BusinessBelongs.
 
   

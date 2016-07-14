2eabe576caedf1771eb501a0e1c60f9c6ac1e3a6
Alessandro Borges Rodrigues <aborgesrodrigues@gmail.com>
2016-07-13 22:39:13 -0300

    fixed bugs

--- pl/persistence_persistence/persistence_persistence_analysis.pl
+++ pl/persistence_persistence/persistence_persistence_analysis.pl
@@ -2,17 +2,24 @@
 
 %% sysout_call(CallId, CallParent, Enclosing, FieldAccess, StaticTypeRef, Argument)
 persistence_persistence_call(CallId) :-
-  callT(CallId, _, MethodCall, _, _, MethodCalled, _, _),
-  methodT(MethodCalled, DAO, _, _, _, _, _, _),
+  callT(CallId, _, MethodCall, Receiver, _, _, _, _),
+  %methodT(MethodCalled, DAO, _, _, _, _, _, _),
   fully_qualified_name(GenericDAO, 'org.sigaept.nucleo.dao.GenericDAO'),
   subtype(DAO, GenericDAO),
-  DAO \== GenericDAO,
-%  not(classT(DAO,_,_,_,_)),
+  %DAO \== GenericDAO,
+  (
+   (identT(Receiver, CallId, MethodCall, Local), localT(Local, _, _, DAO, _, _)) 
+   ; 
+   newT(Receiver, _, _, _, _, _, _, DAO, _)
+   ;
+   callT(Receiver, _, _, _, _, _, _, DAO)
+  ),
   % limit the scope
   methodT(MethodCall, DAOClass, _, _, _, _, _, _),
   %extendsT(_, DAO, GenericDAO),
   subtype(DAOClass, GenericDAO),
   classT(DAOClass,_,_,_,_),
-  DAOClass \== DAO.
+  DAOClass \== DAO,
+  DAO \== GenericDAO.
 %  classT(ClassDAO,_,_,_,_),
 %  extendsT(_, GenericDAO, ClassDAO).
